d0e92e496acb58c153e5b81866b99d590f3b2830
Travis Keep <travisk@vmware.com>
2023-04-28 06:11:15 -0700

    Fix setup.py file.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='wavefront_analytics',
-    version='0.2.3',
+    version='0.2.4',
     url='https://github.com/keep94/wavefront_analytics',
     author='Travis Keep',
     author_email='travisk@vmware.com',
     py_modules=['wavefront_analytics'],
-    install_requires=['wavefront_api_client>=2.179.0', 'urllib3>=1.26.15'],
+    install_requires=['wavefront_api_client>=2.176.0', 'urllib3>=1.26.15'],
 )
